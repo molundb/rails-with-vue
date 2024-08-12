256684b85fdbf78f649a43e8f5ff1dd13a5e0826
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-08-12 11:35:58 -0500

    add index, destroy and complete endpoints

--- api/app/controllers/tasks_controller.rb
+++ api/app/controllers/tasks_controller.rb
@@ -2,6 +2,9 @@ class TasksController < ApplicationController
   before_action :set_post, only: %i[destroy complete]
 
   def index
+    render json: {
+      tasks: Task.all
+    }, status: :ok
   end
 
   def create
@@ -19,9 +22,23 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task.destroy
+
+    render json: {
+      messages: ["Task destroyed successfully"]
+    }, status: :ok
   end
 
   def complete
+    if @task.update(completed: true)
+      render json: {
+        task: @task
+      }, status: :ok
+    else
+      render json: {
+        messages: @task.errors.full_messages
+      }, status: :unprocessable_entity
+    end
   end
 
   private
